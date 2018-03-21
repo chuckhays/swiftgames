8ee0120cf8c83ae0a1039ec2cd4cd05b780bbe31
Chuck Hays <github@chuckhays.net>
2018-03-20 18:18:41 -0700

    Add empty project

---
+++ TicTacToe/TicTacToe/TicTacToe.h
@@ -0,0 +1,19 @@
+//
+//  TicTacToe.h
+//  TicTacToe
+//
+//  Created by Chuck Hays on 3/20/18.
+//  Copyright © 2018 Chuck Hays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TicTacToe.
+FOUNDATION_EXPORT double TicTacToeVersionNumber;
+
+//! Project version string for TicTacToe.
+FOUNDATION_EXPORT const unsigned char TicTacToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TicTacToe/PublicHeader.h>
+
+
