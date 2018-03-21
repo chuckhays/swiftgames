c114aaf3175d59269074868ed3289cb7197664e2
Chuck Hays <github@chuckhays.net>
2018-03-20 18:45:52 -0700

    Test adding file

--- TicTacToe/TicTacToeTests/TicTacToeTests.swift
+++ TicTacToe/TicTacToeTests/TicTacToeTests.swift
@@ -24,6 +24,7 @@ class TicTacToeTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+      let a = Game()
     }
     
     func testPerformanceExample() {
