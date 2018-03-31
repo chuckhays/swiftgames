49d7cba10034a0dd7c83d9df288080432797e522
Chuck Hays <github@chuckhays.net>
2018-03-31 13:46:11 -0500

    Add ==

--- TicTacToe/Sources/TicTacToe/Coordinate.swift
+++ TicTacToe/Sources/TicTacToe/Coordinate.swift
@@ -3,3 +3,11 @@ import Foundation
 public struct Coordinate {
   public var row,col : Int
 }
+
+extension Coordinate: Equatable {}
+
+// MARK: Equatable
+
+func ==<T>(lhs: Coordinate, rhs: Coordinate) -> Bool {
+    return lhs.row == rhs.row && lhs.col == rhs.col
+}
