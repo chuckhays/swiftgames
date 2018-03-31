b2d74f0abfa0166af89b42298143f97aef7f29d1
Chuck Hays <github@chuckhays.net>
2018-03-31 12:12:10 -0500

    Get return status from build/test

--- TicTacToe/build.sh
+++ TicTacToe/build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 pushd  "$(dirname "$0")"
 
-swift build
-swift test
+swift build 
 
-popd
+if [ $? -eq 0 ]
+then
+  echo "Build succeeded"
+else
+  echo "Build failed"
+  exit 1
+fi
+
+swift test
+if [ $? -eq 0 ]
+then
+  echo "Test succeeded"
+else
+  echo "Test failed"
+  exit 1
+fi
